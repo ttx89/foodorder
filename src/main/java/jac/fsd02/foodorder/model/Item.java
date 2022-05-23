973b4633af3f7876931c23b9a965b649b7648665
ssefang <6186588@johnabbottcollege.net>
2022-05-23 09:21:22 -0400

    add item table

--- src/main/java/jac/fsd02/foodorder/model/Item.java
+++ src/main/java/jac/fsd02/foodorder/model/Item.java
@@ -1,16 +1,25 @@
 package jac.fsd02.foodorder.model;
 
 import lombok.*;
+import org.hibernate.annotations.OnDelete;
+import org.hibernate.annotations.OnDeleteAction;
 
+import javax.persistence.*;
 
+@Entity
+@Table(name="tbl_item")
 @AllArgsConstructor
 @NoArgsConstructor
 @Getter
 @Setter
 @Builder
 public class Item {
-
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)//it means auto increment id for the table
     private Long id;
+    @ManyToOne
+    @JoinColumn(name = "category_Id", nullable = false)
+    @OnDelete(action = OnDeleteAction.CASCADE)
     private Category category;
     private String itemName;
     private Double itemPrice;
@@ -18,16 +27,5 @@ public class Item {
     private String description;
     private Active active;
 
-    @Override
-    public String toString() {
-        return "Item{" +
-                "id=" + id +
-                ", category=" + category +
-                ", itemName='" + itemName + '\'' +
-                ", itemPrice=" + itemPrice +
-                ", itemSrc='" + itemSrc + '\'' +
-                ", description='" + description + '\'' +
-                ", active=" + active +
-                '}';
-    }
+
 }
