d97dc0e80ac84520bcdde04056c7f80046f799fe
ssefang <6186588@johnabbottcollege.net>
2022-05-30 11:22:16 -0400

    final

--- src/main/java/jac/fsd02/foodorder/controller/AdminCategoryController.java
+++ src/main/java/jac/fsd02/foodorder/controller/AdminCategoryController.java
@@ -5,11 +5,13 @@ import jac.fsd02.foodorder.model.Category;
 import jac.fsd02.foodorder.service.AdminCategoryService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
+import javax.validation.Valid;
 import java.util.List;
 
 @Controller
@@ -36,23 +38,23 @@ public class AdminCategoryController {
     }
 
     @PostMapping("/save")
-//    public String saveCategory(@Valid @ModelAttribute("category") Category addCategory, BindingResult result){
-//        //if I have validation issues
-//        if (result.hasErrors()){
-//            //I need to stay in the current page !!!!
-//            return "addOrUpdate-category";
-//        }
-//        adminCategoryService.saveOrUpdateCategory(addCategory);
-//
-//        return "redirect:/admin";
-//    }
-
-    public String saveCategory( @ModelAttribute("category") Category addCategory){
-
+    public String saveCategory(@Valid @ModelAttribute("category") Category addCategory, BindingResult result){
+        //if I have validation issues
+        if (result.hasErrors()){
+            //I need to stay in the current page !!!!
+            return "addOrUpdate-category";
+        }
         adminCategoryService.saveOrUpdateCategory(addCategory);
 
         return "redirect:/catogoryList";
     }
+
+//    public String saveCategory( @ModelAttribute("category") Category addCategory){
+//
+//        adminCategoryService.saveOrUpdateCategory(addCategory);
+//
+//        return "redirect:/catogoryList";
+//    }
 
     @GetMapping("/showFormForUpdate/{id}")
     public String updateForm(@PathVariable(value = "id") long id, Model model)throws RecordNotFoundException {
